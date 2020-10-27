1d0a6bb32d010b99b7f0f679db709cd81c2e69fa
dasunkanchana2 <dasunkanchana2@gmail.com>
2020-10-27 20:29:22 +0530

    #Added : bdd automation project

---
+++ digi-automation-client/src/main/java/digi/automation/client/Application.java
@@ -0,0 +1,122 @@
+package digi.automation.client;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        //Create feature file
+        createFeatureFile();
+        //Execute cucumber process
+        executeProcess();
+    }
+
+    /**
+     * This method use to execute test process
+     */
+    private static void executeProcess() {
+        try {
+            cucumber.api.cli.Main.main(new String[]{
+                    "--strict",
+                    "--glue",
+                    "digi.automation.client.steps",
+                    DriverInitializer.getProperty("filePath") + "/login1.feature"});
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+
+    /**
+     * This method use to create feature in the resource folder
+     */
+    private static void createFeatureFile() {
+
+        try {
+            File file = new File(DriverInitializer.getProperty("filePath") + "/login1.feature");
+            if (file.createNewFile()) {
+                writeTestScenarioInTheFile();
+            } else {
+                writeTestScenarioInTheFile();
+            }
+        } catch (IOException e) {
+            System.out.println("Exception Occurred:");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method use to write jerkin commands into feature file
+     */
+    private static void writeTestScenarioInTheFile() {
+        try {
+            FileWriter fw = new FileWriter(DriverInitializer.getProperty("filePath") + "/login1.feature");
+            fw.write("");
+            fw.flush();
+            fw.write(createGherkinScript());
+            fw.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+
+    }
+
+    /**
+     * This method use to create Gherkin script
+     *
+     * @return : String => GherkinScript
+     */
+    private static String createGherkinScript() {
+        JSONObject featureJsonObject = readJsonObjectFromFile();
+        final StringBuilder featureBuilder = new StringBuilder("Feature");
+        featureBuilder.append(":")
+                .append(featureJsonObject.get("Feature")).append("\n")
+                .append("Scenario")
+                .append(":")
+                .append(featureJsonObject.get("Scenario")).append("\n")
+                .append("Given ")
+                .append(featureJsonObject.get("Given")).append("\n")
+                .append("When ")
+                .append(featureJsonObject.get("When")).append("\n")
+                .append("And ")
+                .append(featureJsonObject.get("And")).append("\n")
+                .append("Then ")
+                .append(featureJsonObject.get("Then")).append("\n");
+        return featureBuilder.toString();
+    }
+
+    /**
+     * This method use to read json object
+     *
+     * @return : JsonObject => feature object
+     */
+    private static JSONObject readJsonObjectFromFile() {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            FileReader reader = new FileReader(DriverInitializer
+                    .getProperty("jsonFilePath") + "/feature.json");
+            Object obj = jsonParser.parse(reader);
+            JSONObject featureJsonObject = (JSONObject) obj;
+            jsonObject = featureJsonObject;
+
+        } catch (FileNotFoundException e) {
+            return jsonObject;
+        } catch (IOException e) {
+            return jsonObject;
+        } catch (ParseException e) {
+            return jsonObject;
+        }
+        return jsonObject;
+    }
+
+
+}
+
+
+
---
+++ digi-automation-client/src/main/java/digi/automation/client/steps/LoginSteps.java
@@ -0,0 +1,88 @@
+package digi.automation.client.steps;
+
+import digi.automation.client.DriverInitializer;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginSteps {
+
+    WebDriver webDriver = null;
+
+    @Given("^user launch browser$")
+    public void userLaunchBrowser() throws Throwable {
+        webDriver = DriverInitializer.getDriver(DriverInitializer.getProperty("browser"));
+    }
+
+    @When("^open site landing Page$")
+    public void openSiteLandingPage() throws Throwable {
+        webDriver.get(DriverInitializer.getProperty("login.url"));
+    }
+
+    @When("^click on sign In button$")
+    public void click_on_sign_In_button() throws Throwable {
+        try {
+            //webDriver.wait(12);
+            webDriver.findElement(By.xpath("//*[@id=\"sign_in_btn_link\"]")).click();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            System.out.print(e);
+        }
+    }
+
+    @Then("^user should be able to see the signIn popup window with options$")
+    public void user_should_be_able_to_see_the_signIn_popup_window_with_options() throws Throwable {
+        try {
+            String headerValue = webDriver.findElement(By.xpath("//*[@id=\"sign_in_btn_link\"]")).getText();
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            System.out.print(e);
+        }
+    }
+
+    @Given("^navigate to sign In button and click$")
+    public void navigate_to_sign_In_button_and_click() throws Throwable {
+        System.out.print("Navigate the sign in button and click");
+    }
+
+    @When("^signIn popup window opens$")
+    public void signin_popup_window_opens() throws Throwable {
+        System.out.print("Sign in popup window open");
+    }
+
+    @When("^enter valid email as \"([^\"]*)\" and password as \"([^\"]*)\"$")
+    public void enter_valid_email_as_and_password_as(String arg1, String arg2) throws Throwable {
+        try {
+            webDriver.findElement(By.xpath("//*[@id=\"sign_in_email\"]/input"))
+                    .sendKeys(DriverInitializer.getProperty("username"));
+
+            webDriver.findElement(By.xpath("//*[@id=\"mysecretpassword\"]"))
+                    .sendKeys(DriverInitializer.getProperty("password"));
+
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            System.out.print(e);
+        }
+    }
+
+    @When("^click on sign In button on the sign in modal$")
+    public void click_on_sign_In_button_on_the_sign_in_modal() throws Throwable {
+        try {
+            webDriver.findElement(By.xpath("//*[@id=\"sign_in_btn\"]")).click();
+
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            System.out.print(e);
+        }
+    }
+
+    @Then("^user should be able to see the home page$")
+    public void user_should_be_able_to_see_the_home_page() throws Throwable {
+        try {
+            webDriver.wait(12);
+            webDriver.findElement(By.id("navigate_linkTo_userProfile"));
+            webDriver.quit();
+
+        } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+            System.out.print(e);
+        }
+    }
+}
